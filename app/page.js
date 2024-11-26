706722e24de1c8da59b01fb6b26a960d1c67c648
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2024-11-26 06:29:55 +0300

    edited

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <img src="/bg1.jpg" className="w-full h-[60vh] opacity-20" alt="Açıklama" />
+      <img src="/bg1.jpg" className="w-full h-[60vh] opacity-20" alt="" />
     </div>
   );
 }
